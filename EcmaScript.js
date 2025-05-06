0c52c7d3ca207ce023091c145c50124c26f52e35
chaoukat99 <chawkatomar@gmail.com>
2025-05-06 13:53:43 +0100

    06/05/2025

---
+++ EcmaScript.js
@@ -0,0 +1,101 @@
+
+// Destructuring 
+
+
+
+
+let arr=["Samir","Karim","Khalid"];
+
+
+
+// let user1=arr[0];
+// let user2=arr[1];
+// let user3=arr[2];
+
+
+// let [user1,user2,user3]=arr;
+
+
+// let obj={
+//     username:"Samiri",
+//     age:20,
+//     profession:"Goat"
+// }
+
+
+// let {name,age,profession}=obj;
+
+
+// console.log(username);
+
+
+
+// REST Operator 
+
+let arr1=[1,2,3];
+let arr2=[4,5,6];
+
+
+// let arr3=[...arr1,...arr2]
+// console.log(arr3)
+
+
+
+// console.log(Math.max(...arr2))
+
+
+// find foreach map reduce filter findindex
+
+// HIGHER ORDER FUNCTIONS 
+
+
+
+//  arr.HOF(el,i)=>traitement
+
+
+const tab=["SARA","Salima","OUMNIA","MERYEM","zineb"];
+
+
+// map
+
+
+
+
+// const tab2=tab.map((el,i)=>"Mme "+el);
+// console.log(tab2);
+
+
+
+// filter
+
+// const filteredTab=tab.filter((el,i)=>el!="MERYEM" );
+
+
+// const filteredTab=tab.find((el,i)=>el.includes("M") && el.includes("m"));
+
+
+
+let nums=[1,2,4,20];
+
+
+
+// const Sum=nums.reduce((prev,curr)=>curr*prev)
+
+
+
+tab.forEach((el,i)=>{
+    console.log(el.toUpperCase())
+})
+
+// console.log(Sum);
+
+
+
+
+
+// console.log(filteredTab);
+
+
+
+
+
